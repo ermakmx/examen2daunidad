7f9262d249f5138537438f110b6a2c9b8529c6ad
jose.lopez <150763@utags.edu.mx>
2020-11-25 08:19:28 -0600

    Conexion a BD

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config.js');
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 const app = express();
 
@@ -79,7 +80,23 @@ app.delete('/usuario/:id/:nombre', function(req, res) {
 
     });
 });
+
+mongoose.connect('mongodb://localhost:27017/cafeteria', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}, (err, resp) => {
+
+    if (err) throw err;
+    console.log('Servidor BD Online');
+
+
+});
+
+
 app.listen(process.env.PORT, () => {
+
 
 
     console.log('server online puerto', process.env.PORT);
