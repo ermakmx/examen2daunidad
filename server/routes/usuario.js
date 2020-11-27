f4a0f2a927465f9249783eb83f163db996543bab
jose.lopez <150763@utags.edu.mx>
2020-11-27 09:57:28 -0600

    Post con Registros en base de datos

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Usuario = require('../models/usuario');
 const app = express();
 
 
@@ -26,22 +27,46 @@ app.get('/usuario', function(req, res) {
 
 app.post('/usuario', function(req, res) {
 
-    let nombre = req.body.nombre;
+    // let nombre = req.body.nombre;
+
     let body = req.body;
-    if (nombre === undefined) {
-        res.status(400).json({
-            ok: '400',
-            mensaje: 'Favor de enviar el nombre'
+    let usr = new Usuario({
 
-        });
-    } else {
+        nombre: body.nombre,
+        email: body.email,
+        password: body.password
 
+    });
+
+    usr.save((err, usrDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
         res.json({
-            ok: '200',
-            mensaje: 'usuario insertado',
-            body: body
+            ok: true,
+            msg: 'Usuario insertado con exito',
+            usrDB
+
         });
-    }
+
+    });
+    // if (nombre === undefined) {
+    //     res.status(400).json({
+    //         ok: '400',
+    //         mensaje: 'Favor de enviar el nombre'
+
+    //     });
+    // } else {
+
+    //     res.json({
+    //         ok: '200',
+    //         mensaje: 'usuario insertado',
+    //         body: body
+    //     });
+    // }
 });
 
 
