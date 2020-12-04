dcb2c103da481b71dff5b68176e3b2deb111426b
jose.lopez <150763@utags.edu.mx>
2020-12-04 16:18:51 -0600

    Conexion con mongo en la nube con exito

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(require('./routes/usuario'));
 app.use(require('./routes/categoria'));
 app.use(require('./routes/producto'));
 app.use(require('./routes/login'));
-mongoose.connect('mongodb://localhost:27017/cafeteria', {
+mongoose.connect('mongodb+srv://admin:contraexamen@cluster0.g8ksu.mongodb.net/cafeteria', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
